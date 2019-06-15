8b87b40c32af5a649a002f0ea25f32c6176b51a4
fpatrick <39106404+fpatrick@users.noreply.github.com>
2019-06-15 19:26:23 +0100

    Added Entity Framework

---
+++ MyShop/MyShop.DataAccess/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess
+{
+    public class DataContext : DbContext //DbContext (from entity) includes all the base methods to our datacontext to work
+    {
+        public DataContext()    //Construct to capture and passes the database string DbContext  that expect
+            : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }  //Tells which models will be stored in tables, because we dont want to store our view models in the database
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+
+        //Next we go to View > Other Windows > Package Manager Console.   Change default projetc to DataAccess.SQL
+        //Right click WebUI and click Set as Startup Project
+        //On console 1: Enable-Migrations; 2: Add-Migration Initial (can be any name); 3: Update-Database
+    }
+}
