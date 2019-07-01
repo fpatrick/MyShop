22d3abdeb9660c501a81238a9788219066fe4db5
fpatrick <39106404+fpatrick@users.noreply.github.com>
2019-07-01 17:40:31 +0100

    Added testing

--- MyShop/MyShop.WebUI.Tests/Controllers/HomeControllerTest.cs
+++ MyShop/MyShop.WebUI.Tests/Controllers/HomeControllerTest.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
 using MyShop.WebUI;
 using MyShop.WebUI.Controllers;
 
@@ -13,42 +16,20 @@ namespace MyShop.WebUI.Tests.Controllers
     public class HomeControllerTest
     {
         [TestMethod]
-        public void Index()
+        public void IndexPageDoesReturnProducts()
         {
-            // Arrange
-            //HomeController controller = new HomeController();
+            IRepository<Product> productContext = new Mocks.MockContext<Product>();   //instead of a new inmemory or sql
+            IRepository<ProductCategory> productCategoryContext = new Mocks.MockContext<ProductCategory>();
 
-            //// Act
-            //ViewResult result = controller.Index() as ViewResult;
+            productContext.Insert(new Product());
 
-            //// Assert
-            //Assert.IsNotNull(result);
+            HomeController controller = new HomeController(productContext, productCategoryContext);
+
+            var result = controller.Index() as ViewResult; //now we can call the index method on the controller to get the results
+            var viewModel = (ProductListViewModel)result.ViewData.Model; //AS parte of view result it will contain a product list viewmodel
+
+            Assert.AreEqual(1, viewModel.Products.Count());
         }
 
-        [TestMethod]
-        public void About()
-        {
-            //// Arrange
-            //HomeController controller = new HomeController();
-
-            //// Act
-            //ViewResult result = controller.About() as ViewResult;
-
-            //// Assert
-            //Assert.AreEqual("Your application description page.", result.ViewBag.Message);
-        }
-
-        [TestMethod]
-        public void Contact()
-        {
-            //// Arrange
-            //HomeController controller = new HomeController();
-
-            //// Act
-            //ViewResult result = controller.Contact() as ViewResult;
-
-            //// Assert
-            //Assert.IsNotNull(result);
-        }
     }
 }
---
+++ MyShop/MyShop.WebUI.Tests/Mocks/MockContext.cs
@@ -0,0 +1,78 @@
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.WebUI.Tests.Mocks
+{
+    public class MockContext<Generic> : IRepository<Generic> where Generic : BaseEntity
+    {
+        List<Generic> items;
+        string className; //To help handle how we store objects in cache
+
+        public MockContext()
+        {
+            items = new List<Generic>();
+        }
+
+        public void Commit()
+        {
+            return;
+        }
+
+        public void Insert(Generic generic)
+        {
+            items.Add(generic);
+        }
+
+        public void Update(Generic generic)
+        {
+            Generic genericToUpdate = items.Find(i => i.Id == generic.Id);
+
+            if (genericToUpdate != null)
+            {
+                genericToUpdate = generic;
+            }
+            else
+            {
+                throw new Exception(className + " not found!");
+            }
+        }
+
+        public Generic Find(String Id)
+        {
+            Generic generic = items.Find(i => i.Id == Id);
+            if (generic != null)
+            {
+                return generic;
+            }
+            else
+            {
+                throw new Exception(className + " not found!");
+            }
+        }
+
+        public IQueryable<Generic> Collection()
+        {
+            return items.AsQueryable();
+        }
+
+        public void Delete(string Id)
+        {
+            Generic genericToDelete = items.Find(i => i.Id == Id);
+
+            if (genericToDelete != null)
+            {
+                items.Remove(genericToDelete);
+            }
+            else
+            {
+                throw new Exception(className + " not found!");
+            }
+
+        }
+    }
+}
